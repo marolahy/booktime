15fe0619516b95a560ba47ee5ed590aded35761e
marolahy bezama Randriamifidy <marolahy@gmail.com>
2019-11-07 21:25:06 +0100

    [statics] ajout de envoie mail

---
+++ main/tests/test_forms.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from django.core import mail
+from main import forms
+class TestForms(TestCase):
+    def test_valid_contact_us_form_sends_email(self):
+        form = forms.ContactForm({
+            'name': 'Maitre Yoda',
+            'message': 'Que la force soit avec toi'
+        })
+        self.assertTrue(form.is_valid())
+
+        with self.assertLogs('main.forms',level='INFO') as cm:
+            form.send_mail()
+
+        self.assertEqual(len(mail.outbox),1)
+        self.assertEqual(mail.outbox[0].subject,'Test mail')
+        self.assertGreaterEqual(len(cm.output),1)
+    
+    def test_invalid_contact_us_form(self):
+        form = forms.ContactForm({
+            'message': 'Que la force soit avec toi'
+        })
+
+        self.assertFalse(form.is_valid())
+        
