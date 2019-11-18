317c402e6d644fa4ec4de5abd83142a357d86d8f
marolahy bezama Randriamifidy <marolahy@gmail.com>
2019-11-18 14:04:03 +0100

    [database] ajout detailview

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,20 @@
 
 from django.urls import path
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
 from main import views
 from django.conf import settings
 from django.conf.urls.static import static
+from main import models
 
 urlpatterns = [
     path("products/<slug:tag>/",
         views.ProductListView.as_view(),
         name="products"
+    ),
+    path("product/<int:pk>/",
+        DetailView.as_view(model=models.Product),
+        name="product"
     ),
     path('contact-us',
         views.ContactUsView.as_view(template_name="contact_form.html"),
