38915421415a65598fa4d35b7b31bed28e100bd8
marolahy bezama Randriamifidy <marolahy@gmail.com>
2019-11-19 11:23:41 +0100

    [login] ajout de formulaire d'ajout d'addresse

--- main/views.py
+++ main/views.py
@@ -1,4 +1,11 @@
-from django.views.generic.edit import FormView
+from django.views.generic.edit import (
+    FormView,
+    CreateView,
+    UpdateView,
+    DeleteView
+    )
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.shortcuts import get_object_or_404
 import logging
@@ -74,5 +81,56 @@ class SignupView(FormView):
 
         return response
 
+class AddressListView(LoginRequiredMixin, ListView):
+    model = models.Address
+
+    def get_queryset(self):
+        return self.model.objects.filter( user=self.request.user )
+
+class AddressCreateView( LoginRequiredMixin, CreateView ):
+    model = models.Address
+    fields = [
+        'name',
+        'address1',
+        'address2',
+        'zip_code',
+        'city',
+        'country',
+    ]
+    success_url = reverse_lazy("address_list")
+
+    def form_valid( self, form):
+        obj = form.save( commit=False )
+        obj.user = self.request.user
+        obj.save()
+        return super().form_valid(form)
+
+class AddressUpdateView( LoginRequiredMixin, UpdateView ):
+    model = models.Address
+
+    fields = [
+        'name',
+        'address1',
+        'address2',
+        'zip_code',
+        'city',
+        'country',
+    ]
+
+    success_url = reverse_lazy("address_list")
+
+    def get_queryset(self):
+        return self.model.objects.filter( user=self.request.user )
+    
+
+class AddressDeleteView(LoginRequiredMixin, DeleteView):
+    model = models.Address
+    success_url = reverse_lazy("address_list")
+
+    def get_queryset(self):
+        return self.model.objects.filter( user=self.request.user )
+    
+
+    
         
     
